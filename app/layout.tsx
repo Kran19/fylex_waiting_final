b3a715ae3f70fdc45a25bde7d7f80392883a60f1
Kran19 <karanmack5356@gmail.com>
2026-07-20 11:03:35 +0530

    feat: initialize root layout with global styles, Meta Pixel tracking, and responsive spotlight component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Playfair_Display } from "next/font/google";
 import "./globals.css";
 import { Spotlight } from "@/components/ui/spotlight";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -42,6 +43,31 @@ export default function RootLayout({
           2. Added w-full and min-h-screen to force the mouse listeners to read the entire screen.
       */}
       <body className="w-full min-h-screen relative overflow-x-clip m-0 p-0 text-white bg-black/[0.96]">
+        {/* Meta Pixel Code */}
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1035472045697724');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1035472045697724&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
         
         {/* GLOBAL LAYER: Now accurately maps your full monitor space */}
         <Spotlight className="z-0" size={550} />
